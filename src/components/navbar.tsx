5067057e397c9b64c05b8ca878551674848b30e4
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2023-04-28 14:03:22 +0700

    navbar responsive css finished.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ import { signOut} from "firebase/auth";
 import { auth } from "../config/firebase";
 import { useNavigate } from "react-router-dom";
 import {useAuthState} from "react-firebase-hooks/auth";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/navbar.css"
 
 
@@ -10,7 +10,8 @@ export const Navbar = () => {
   const navigate = useNavigate(); 
   const [user] = useAuthState(auth);
   const [cssStyle, setStyle] = useState<string>("hidden")
-
+  const [smallNavbar, setNavbar] = useState<boolean>(false)
+  
   const logOut = async () => {
     try{
         await signOut(auth)
@@ -20,24 +21,54 @@ export const Navbar = () => {
         console.error(err)
     }
   }
-  function checkDimensions() {
-    const { innerWidth: width} = window;
-    console.log(width)
+  function checkSmallNavbarStyle() {
+    if (cssStyle === "hidden"){
+      setStyle("")
+    }else{
+      setStyle("hidden")
+    }
   }
 
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 965){
+        setNavbar(true)
+      }else{
+        setNavbar(false)
+      } 
+    }
+    window.addEventListener('resize', handleResize) 
+  });
+
   return (
-    
+    <div>
+    { smallNavbar ?
+      <nav className="navbar">
+        <ul className="navbar-nav">
+          <li className="brand" style={{display: "flex"}}><h1>Mercury</h1><button onClick={checkSmallNavbarStyle} className="nav-btn">=</button></li>
+          
+          <li className={`nav-item ${cssStyle}`}><a className="nav-link" href="/dashboard">Dashboard</a></li>
+          <li className={`nav-item ${cssStyle}`}><a className="nav-link" href="/profile">Profile</a></li>
+          <li className={`nav-item ${cssStyle}`}><a className="nav-link" href="/register">Register</a></li>
+          <li className={`nav-item ${cssStyle}`}><a className="nav-link" href="/login">Login</a></li>
+          <li className={`nav-item ${cssStyle}`}><button className="dark-btn ml-5 mr-5" onClick={logOut}>Sign Out</button></li>
+        </ul>
+      </nav>
+    :
     <nav className="navbar">
       <ul className="navbar-nav">
-        <li className="brand" style={{display: "flex"}}><h1>Mercury</h1><button onClick={checkDimensions} className="dark-btn nav-btn">V</button></li>
+        <li className="brand"><h1>Mercury</h1></li>
         
-        <li className="nav-item"><a className={`nav-link ${cssStyle}`} href="/dashboard">Dashboard</a></li>
+        <li className="nav-item"><a className="nav-link" href="/dashboard">Dashboard</a></li>
         <li className="nav-item"><a className="nav-link" href="/profile">Profile</a></li>
         <li className="nav-item"><a className="nav-link" href="/register">Register</a></li>
         <li className="nav-item"><a className="nav-link" href="/login">Login</a></li>
-        <li className="nav-item"><button className="dark-btn ml-5 mr-5" onClick={logOut}>Sign Out</button></li>
+        <li className="nav-item"><button className=" dark-btn ml-5 mr-5" onClick={logOut}>Sign Out</button></li>
       </ul>
     </nav>
+  }
+  </div>
+    
   )
 }
 
